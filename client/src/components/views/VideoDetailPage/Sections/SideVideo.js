9eb6783cfda6f025a0256646aeac12fe6ad70bc1
김성현 <61579430+kim70273@users.noreply.github.com>
2021-07-26 14:31:10 +0900

    side video 생성완료

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 
 const SideVideo = () => {
 
-    const [sideVideos, setSideVideos] = useState();
+    const [sideVideos, setSideVideos] = useState([]);
 
     //비디오 정보 가져오기위해 useEffect사용
     useEffect(() => {
@@ -18,26 +18,40 @@ const SideVideo = () => {
         })
     }, []);
 
-    return (
-        <div style={{display:'flex', marginBottom:'1rem', padding:'0 2rem'}}>
+    //여러개의 side video 카드가 필요하니까 map 메소드를 이용한다.
+    const renderSideVideo = sideVideos.map((video, index) => {
+
+        let minutes = Math.floor(video.duration / 60);
+        let seconds = Math.floor((video.duration - minutes * 60));
+
+        return <div key={index} style={{display:'flex', marginBottom:'1rem', padding:'0 2rem'}}>
             
-            <div style={{width: '40%', marginBottom:'1rem'}}>
+            <div style={{width: '40%', marginRight:'1rem'}}>
                 <a href>
-                    <img style={{width:'100%'}} src alt />
+                    <img style={{width:'100%', height:'100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
                 </a>
             </div>
 
             <div style={{width:'50%'}}>
-                <a href>
-                    <span style={{fontSize:'1rem', color:'black'}}>videoTitle</span><br />
-                    <span>작성자</span><br />
-                    <span>조회수</span><br />
-                    <span>시간</span>
+                <a href style={{color:'gray'}}>
+                    <span style={{fontSize:'1rem', color:'black'}}>{video.title}</span><br />
+                    <span>{video.writer.name} </span><br />
+                    <span>{video.views} views </span><br />
+                    <span>{minutes} : {seconds} </span>
                 </a>
             </div>
 
 
         </div>
+    })
+
+    return (
+        <React.Fragment>
+
+            {renderSideVideo}
+
+        </React.Fragment>
+        
     )
 }
 
